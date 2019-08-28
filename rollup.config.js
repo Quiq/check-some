b4dec205019957131b6817d43d51f7e0deb80878
Nate Norberg <nate.norberg@goquiq.com>
2019-08-28 17:12:32 -0600

    Got it to start building at least

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,33 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
 
-import pkg from './package.json'
+import pkg from './package.json';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default {
   input: 'src/index.js',
   output: [
     {
       file: pkg.main,
-      format: 'cjs'
+      format: 'cjs',
     },
     {
       file: pkg.module,
-      format: 'es'
-    }
+      format: 'es',
+    },
   ],
-  external: [
-    'react',
-    'react-dom',
-    'prop-types'
-  ],
+  external: ['react', 'react-dom', 'prop-types'],
   plugins: [
+    resolve({extensions}),
     postcss({}),
     babel({
-      exclude: 'node_modules/**'
+      extensions,
+      exclude: 'node_modules/**',
+      include: ['src/**/*'],
     }),
-    resolve(),
-    commonjs()
-  ]
-}
+    commonjs(),
+  ],
+};
