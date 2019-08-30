54ed05730e13573de9718f8fe3addb627c3108a4
Nate Norberg <nate.norberg@goquiq.com>
2019-08-30 09:51:14 -0600

    Using new context API

--- src/Field.tsx
+++ src/Field.tsx
@@ -1,49 +1,31 @@
-import React from 'react';
-import * as PropTypes from 'prop-types';
-import {CHECK_SOME_CONTEXT} from './globals';
+import React, {useState, useContext} from 'react';
+import {CheckSomeContext} from './CheckSome';
 import {ValidationErrors} from './globals';
 
 type CheckSomeFieldChildProps<T> = {
-  value: T,
-  errors: ValidationErrors | null,
-  touched: boolean,
-  valid: boolean,
+  value: T;
+  errors: ValidationErrors | null;
+  touched: boolean;
+  valid: boolean;
 };
 
 type Props = {
-  name: string,
-  children: (props: CheckSomeFieldChildProps<any>) => React.ReactNode,
+  name: string;
+  children: (props: CheckSomeFieldChildProps<any>) => React.ReactNode;
 };
 
-type State = {
-  touched: boolean,
+const CheckSomeField = ({name, children}: Props) => {
+  const [touched, setTouched] = useState(false);
+  const {values, errors: formErrors} = useContext(CheckSomeContext);
+  const value = values[name];
+  const errors = formErrors ? formErrors[name] : null;
+  const valid = !errors;
+
+  return (
+    <div className="CheckSomeField" onBlur={() => setTouched(true)}>
+      {children({value, errors, touched, valid})}
+    </div>
+  );
 };
 
-export default class CheckSomeField extends React.Component<Props, State> {
-  state = {
-    touched: false,
-  };
-
-  static contextTypes = {
-    [CHECK_SOME_CONTEXT]: PropTypes.object.isRequired,
-  };
-
-  markFieldTouched = () => {
-    this.setState({touched: true});
-  };
-
-  render() {
-    const {values, errors: formErrors} = this.context[CHECK_SOME_CONTEXT];
-    const {name} = this.props;
-    const {touched} = this.state;
-    const value = values[name];
-    const errors = formErrors ? formErrors[name] : null;
-    const valid = !errors;
-
-    return (
-      <div className="CheckSomeField" onBlur={this.markFieldTouched}>
-        {this.props.children({value, errors, touched, valid})}
-      </div>
-    );
-  }
-}
+export default CheckSomeField;
