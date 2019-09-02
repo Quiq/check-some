be8c1d1e6ea013862b55888ea4179e64d02a9611
Nate Norberg <nate.norberg@goquiq.com>
2019-09-01 18:56:41 -0600

    Making some progress i think

--- src/CheckSome.tsx
+++ src/CheckSome.tsx
@@ -3,32 +3,28 @@ import isEqual from 'lodash/isEqual';
 import CheckSomeField from './Field';
 import {ValidationErrors} from './globals';
 
-type ValidationRule = (value: any) => ValidationErrors | null;
+type ValidationRule<T> = (value: T) => ValidationErrors | null;
 
-type ValidationGroupRules = {[name: string]: Array<ValidationRule>};
-type ValidationGroupErrors = {[name: string]: ValidationErrors} | null;
+type ValidationGroupValues<T> = {[K in keyof T]: T[K]};
+type ValidationGroupRules<T> = Partial<{[K in keyof T]: Array<ValidationRule<T[K]>>}>;
+type ValidationGroupErrors<T> = Partial<{[K in keyof T]: ValidationErrors}> | null;
 
-export type CheckSomeChildProps = {
+export type CheckSomeChildProps<T> = {
   valid: boolean;
   changed: boolean;
-  errors: ValidationGroupErrors;
+  errors: ValidationGroupErrors<T>;
 };
 
-export type CheckSomeProps = {
-  rules: ValidationGroupRules;
-  values: {[key: string]: any}; // TODO: Get a better type here
-  initialValues?: {[key: string]: any};
-  children: (props: CheckSomeChildProps) => React.ReactNode;
+export type CheckSomeProps<T> = {
+  rules: ValidationGroupRules<T>;
+  values: ValidationGroupValues<T>;
+  initialValues?: ValidationGroupValues<T>;
+  children: (props: CheckSomeChildProps<T>) => React.ReactNode;
 };
 
-interface Context {
-  values: {[key: string]: any};
-  errors: ValidationGroupErrors;
-}
+export const CheckSomeContext = createContext({});
 
-export const CheckSomeContext = createContext<Context>({values: {}, errors: {}});
-
-export default class CheckSome extends React.Component<CheckSomeProps> {
+export default class CheckSome<T> extends React.Component<CheckSomeProps<T>> {
   static Field = CheckSomeField;
 
   static defaultProps = {
@@ -49,13 +45,15 @@ export default class CheckSome extends React.Component<CheckSomeProps> {
     return this.initialValues;
   };
 
-  getErrors = (): ValidationGroupErrors =>
-    Object.keys(this.props.rules).reduce((errors: ValidationGroupErrors, key) => {
+  getErrors = (): ValidationGroupErrors<T> =>
+    Object.keys(this.props.rules).reduce((errors: ValidationGroupErrors<T>, keyValue) => {
+      // @ts-ignore
+      const key: keyof T = keyValue;
       const rules = this.props.rules[key];
       const value = this.props.values[key];
 
-      const newErrors = rules.reduce(
-        (e: ValidationErrors | null, rule: ValidationRule) => e || rule(value),
+      const newErrors = rules!.reduce(
+        (e: ValidationErrors | null, rule: ValidationRule<T[typeof key]>) => e || rule(value),
         null,
       );
 
@@ -87,3 +85,29 @@ export default class CheckSome extends React.Component<CheckSomeProps> {
     );
   }
 }
+
+const greaterThanZero = (value: number) => {
+  return value > 0 ? null : {greaterThanZero: {value}};
+};
+
+const startsWith = (startingText: string) => (value: string) =>
+  value.startsWith(startingText) ? null : {startsWith: {startingText, value}};
+
+const tsCheck = (
+  <CheckSome
+    values={{
+      stringField: 'string',
+      numberField: 7,
+    }}
+    initialValues={{
+      stringField: 'string',
+      numberField: 7,
+    }}
+    rules={{
+      stringField: [startsWith('test')],
+      numberField: [greaterThanZero],
+    }}
+  >
+    {() => <div />}
+  </CheckSome>
+);
